9f485c144629c7a57598f4a60735b219343e1c60
johngillard15 <johngillard15@gmail.com>
2021-08-31 01:39:20 -0400

    fixed speed calclations

--- src/com/game/DragRace.java
+++ src/com/game/DragRace.java
@@ -7,12 +7,11 @@ import com.vehicles.SportsCar;
 import com.vehicles.Vehicle;
 import com.vehicles.engines.Engine;
 
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DragRace {
-    private List<Vehicle> vehicles = new ArrayList<>();
+    private final List<Vehicle> vehicles = new ArrayList<>();
 
     public DragRace(){
         Engine nitromethane = new Engine("Nitromethane", 10_000, 505);
@@ -38,27 +37,28 @@ public class DragRace {
     }
 
     public void run(){
-//        for(Vehicle vehicle : vehicles){
-//            System.out.println(vehicle);
-//        }
-
         System.out.println("Which car would you like to drive?");
         int listNum = 0;
         for(Vehicle vehicle : vehicles)
             System.out.printf("%d. %s\n", ++listNum, vehicle.name);
         int choice = Input.getInt(1, vehicles.size()) - 1;
-
         Vehicle car = vehicles.get(choice);
+
         int distance = 0;
-        int turnSpeed = (int)(car.getSpeed() / car.getElapsedTime());
-        int currentSpeed = 0;
-        double totalTime = 0;
+        int fps = (int)(1320 / car.elapsedTime);
+        double turnSpeed = car.trapSpeed / car.elapsedTime;
+        double currentSpeed = 0;
+        double currentTime = 0;
 
         System.out.printf("\n%s, start your engine...\n", car.name);
         while(distance < 1320){
+            System.out.printf("\nCurrent speed: %.2f mph", currentSpeed);
+            System.out.printf("\n%s now at %,d feet\n", car.name, distance);
+            System.out.printf("Time: %.2f s\n", currentTime);
+
             System.out.println("What would you like to do?");
             System.out.println("1. accelerate");
-            System.out.println("2. coast");
+            System.out.println("2. cruise");
             System.out.println("3. brake");
             choice = Input.getInt(1, 3);
 
@@ -66,34 +66,33 @@ public class DragRace {
                 case 1 -> {
                     System.out.println("Speeding up...");
                     currentSpeed += turnSpeed;
-                    totalTime += car.getSpeed();
                 }
                 case 2 -> {
                     System.out.println("Maintaining speed...");
-                    totalTime += car.getElapsedTime() / car.getSpeed(); // FIXME: 8/30/2021 get real time
                 }
                 case 3 -> {
                     System.out.println("Slowing down...");
 
                     currentSpeed -= turnSpeed;
-                    if(currentSpeed < 0)
-                        currentSpeed = 0;
-                    totalTime += car.getElapsedTime() / car.getSpeed();
+                    if(currentSpeed < 0) currentSpeed = 0;
                 }
             }
 
-            distance += currentSpeed;
+            currentTime += 1;
+            if(currentSpeed > 0) distance += fps;
 
-            if(distance >= 1320)
-                System.out.printf("\n%s has finished the race in %.2f s.\n", car.name, totalTime);
-            else {
-                System.out.printf("\nCurrent speed: %d mph", currentSpeed);
-                System.out.printf("\n%s now at %,d feet\n", car.name, distance);
+            if(distance >= 1320) {
+                double overTime = 1 - ((distance - 1320) / (double)fps);
+                currentTime -= overTime;
+                currentSpeed -= turnSpeed - (turnSpeed * overTime);
+
+                System.out.printf("\n%s has finished the race in %.2f s, with a trap speed of %.2f mph.\n",
+                        car.name, currentTime, currentSpeed);
             }
         }
 
         System.out.println("Now braking...");
-        double brakingTime = car.getTrapSpeed() / car.getBrakingDistance();
+        double brakingTime = car.trapSpeed / car.brakingDistance;
         System.out.printf("%s has stopped in %.2f s.\n", car.name, brakingTime);
     }
 }
--- src/com/vehicles/Vehicle.java
+++ src/com/vehicles/Vehicle.java
@@ -6,50 +6,20 @@ public abstract class Vehicle {
     public final String name;
     public final Engine engine;
     public final int weight;
-    protected double trapSpeed;
-    protected double elapsedTime;
-    public int speed = 0;
-    protected int brakingDistance;
+    public final double trapSpeed;
+    public final double elapsedTime;
+    public final int brakingDistance;
 
     public Vehicle(String name, Engine engine, int weight){
         this.name = name;
         this.engine = engine;
         this.weight = weight + engine.weight;
 
-        calculateTrapSpeed();
-        calculateElapsedTime();
-        speed += 1320 / (int)elapsedTime;
-        setBrakingDistance();
-    }
-
-    protected void calculateTrapSpeed(){
         // Hale equation
         trapSpeed = Math.cbrt((double)engine.horsePower / (double)weight) * 234;
-    }
-
-    protected void calculateElapsedTime(){
         // Hale equation
         elapsedTime = Math.cbrt((double)weight / (double)engine.horsePower) * 5.825;
-    }
 
-    public double getTrapSpeed(){
-        return trapSpeed;
-    }
-
-    public double getElapsedTime() {
-        return elapsedTime;
-    }
-
-    public int getSpeed(){
-        return speed;
-    }
-
-
-    public int getBrakingDistance(){
-        return brakingDistance;
-    }
-
-    public void setBrakingDistance() {
         brakingDistance = (int)Math.sqrt(trapSpeed / 10);
     }
 
